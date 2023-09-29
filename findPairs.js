14d20d8bc5cd66c0ed56d0380e6cd27a3212d59a
Laura Orozco <lauorozco13@gmail.com>
2023-09-29 17:10:25 -0500

    main / Minor fix for semicolons

--- findPairs.js
+++ findPairs.js
@@ -1,29 +1,29 @@
 // Function to find pairs of integers from a list that sum to a given value
 function findPairs(list, target) {
-    const pairs = []
+    const pairs = [];
 
-    const seen = new Set()
+    const seen = new Set();
 
     for (const item of list) {
-        const neededNumber = target - item
+        const neededNumber = target - item;
 
         if (seen.has(neededNumber)) {
-            pairs.push([item, neededNumber])
+            pairs.push([item, neededNumber]);
         } else {
-            seen.add(item)
+            seen.add(item);
         }
     }
 
-    return pairs
+    return pairs;
 }
 
-const args = process.argv.slice(2)
-const list = args[0].split(',').map(Number)
-const target = parseInt(args[1], 10)
+const args = process.argv.slice(2);
+const list = args[0].split(',').map(Number);
+const target = parseInt(args[1], 10);
 
-const result = findPairs(list, target)
+const result = findPairs(list, target);
 result.forEach(pair => {
-    console.log(`+ ${pair[0]}, ${pair[1]}`)
+    console.log(`+ ${pair[0]}, ${pair[1]}`);
 });
 
 module.exports = findPairs;
